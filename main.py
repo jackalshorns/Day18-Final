437162616d4d6184f48cbcf4af027c26c07cca70
jackalshorns <jason@uxisfine.com>
2024-05-18 14:14:46 -0500

    Completed with same colors each row

--- main.py
+++ main.py
@@ -1,19 +1,52 @@
 import colorgram
+from turtle import Turtle, Screen
+
+tim = Turtle()
+scr = Screen()
 
 colors = colorgram.extract('20_001.jpg', 25)
 
-rgb_colors = []
+color_list = []
+
+tim.speed("fastest")
+tim.shape("circle")
+tim.penup()
+scr.colormode(255)
+hor = 0
+
 for color in colors:
-    print(color.rgb)
-    r = color.rgb
+    r = color.rgb.r
     g = color.rgb.g
     b = color.rgb.b
     new_rgb_color = (r, g, b)
-    rgb_colors.append(new_rgb_color)
+    color_list.append(new_rgb_color)
 
-print(rgb_colors)
+tim.setpos(-200, -200)
+ver = 0
+hor = 0
+
+while ver <= 9:
+    while hor <= 10:
+        r = int(color_list[hor][0])
+        g = int(color_list[hor][1])
+        b = int(color_list[hor][2])
+        tim.color(r, g, b)
+        print(r, g, b)
+        if hor == 9 and ver == 9:
+            break
+        elif hor == 10:
+            tim.bk(550)
+            tim.left(90)
+            tim.fd(50)
+            tim.right(90)
+            #tim.color(255,255,255)
+        else:
+            tim.stamp()
+        tim.fd(50)
+        hor += 1
+    hor = 0
+    ver += 1
 
 
 
-
-
+scr.exitonclick()
